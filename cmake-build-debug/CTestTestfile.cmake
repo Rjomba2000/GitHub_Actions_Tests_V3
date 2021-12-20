ce0d79a7d9b6bb2109a27a7a7ad9ef8e129ab52a
Денис Егошин <egoshin.deniska@mail.ru>
2021-12-21 01:35:26 +0300

    Починил gitignore 3

--- cmake-build-debug/CTestTestfile.cmake
+++ cmake-build-debug/CTestTestfile.cmake
@@ -4,5 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test([=[FirstTest]=] "tests")
+add_test([=[FirstTest]=] "tests.exe")
 set_tests_properties([=[FirstTest]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/egosh/Desktop/ck/github_actions_v3/CMakeLists.txt;10;add_test;C:/Users/egosh/Desktop/ck/github_actions_v3/CMakeLists.txt;0;")
