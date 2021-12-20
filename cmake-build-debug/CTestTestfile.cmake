adfa1ab30c215b7bee7678369d5f3f4b3c8faf76
Денис Егошин <egoshin.deniska@mail.ru>
2021-12-20 22:00:54 +0300

    Добавил программу

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/egosh/Desktop/ck/github_actions_v3
+# Build directory: C:/Users/egosh/Desktop/ck/github_actions_v3/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
